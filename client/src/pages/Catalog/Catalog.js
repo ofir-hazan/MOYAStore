15cff5f2177e7666601c35aed6744c2f6a880330
gmesh <gmeshie@gmail.com>
2023-02-15 21:40:06 +0200

    Search and filter the catalog products

--- client/src/pages/Catalog/Catalog.js
+++ client/src/pages/Catalog/Catalog.js
@@ -1,6 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Catalog.css";
 import { styled } from "@mui/material/styles";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Product from "../../Components/Product/Product";
 import StatisticsDialog from "../../Components/StatisticsDialog/StatisticsDialog";
 import BarChartIcon from "@mui/icons-material/BarChart";
@@ -14,9 +19,17 @@ function Catalog(props) {
   const { connectedUser } = useContext(GlobalContext);
   const { products } = props;
   const [statsDialogOpen, setStatsDialogOpen] = useState(false);
+  const [filterValue, setFilterValue] = useState("");
+  const [displayProducts, setDisplayProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [catFilterValue, setCatFilterValue] = useState("");
+  const [suppliers, setSuppliers] = useState([]);
+  const [supFilterValue, setSupFilterValue] = useState("");
+  const [maxRangeValue, setMaxRangeValue] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
 
   function renderProducts() {
-    return products.map((product) => (
+    return displayProducts.map((product) => (
       <Product
         key={product.id}
         product={product}
@@ -34,6 +47,62 @@ function Catalog(props) {
     },
   });
 
+  const handleSliderChange = (event, newValue) => {
+    setMaxRangeValue(newValue);
+  };
+
+  const getMaxPrice = () => {
+    let maxPrice = products[0].price;
+    for (let index = 1; index < products.length; index++) {
+      if (products[index].price > maxPrice) {
+        maxPrice = products[index].price;
+      }
+    }
+    setMaxPrice(maxPrice);
+  }
+
+  useEffect(() => {
+    setDisplayProducts(...products)
+    getMaxPrice();
+    fetch("http://localhost:3001/suppliers/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setSuppliers(data);
+      })
+      .catch((err) => console.log(err));
+    fetch("http://localhost:3001/categories/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    let allProducts = [...products];
+    let filteredProducts = [...allProducts];
+
+    if (filterValue) {
+      filteredProducts = filteredProducts.filter((product) => product.name.toLowerCase().includes(filterValue.toLowerCase()));
+    } 
+
+    if (catFilterValue) {
+      const category = categories.find((cat) => cat.name.toLowerCase().includes(catFilterValue.toLowerCase()));
+      filteredProducts = filteredProducts.filter((product) => product.categoryId === category?._id);
+    }
+
+    if (supFilterValue) {
+      const supplier = suppliers.find((sup) => sup.name.toLowerCase().includes(supFilterValue.toLowerCase()));
+      filteredProducts = filteredProducts.filter((product) => product.supplierId === supplier?._id);
+    }
+
+    if (maxRangeValue !== 0) {
+      filteredProducts = filteredProducts.filter((product) => product.price <= maxRangeValue);
+    }
+
+    setDisplayProducts(filteredProducts);
+  }, [filterValue, catFilterValue, supFilterValue, maxRangeValue]);
+
   return (
     <div className="products">
       <div className="productsHeader">
@@ -45,13 +114,56 @@ function Catalog(props) {
             Watch statictics
           </ColorButton>
         )}
-        <div className="productsTitle">רשימת מוצרים</div>
+        <div className="productsTitle">Products</div>
+      </div>
+      <div className="searchFilterContainer">
+        <div>
+          <Autocomplete
+            freeSolo
+            value={filterValue}
+            onChange={(e, newValue) => setFilterValue(newValue)}
+            options={products.map((product) => product.name)}
+            renderInput={(params) => <TextField {...params} label="Search for an item..." />}
+          />
+        </div>
+        <div className="filterContainer">
+          <Autocomplete
+            freeSolo
+            value={supFilterValue}
+            onChange={(e, newValue) => setSupFilterValue(newValue)}
+            options={suppliers.map((supplier) => supplier.name)}
+            sx={{width:200}}
+            renderInput={(params) => <TextField {...params} label="Filter by supplier" />}
+          />
+          <div>
+            <Box sx={{width:300}}>
+              <Typography id="input-slider" gutterBottom>
+                Filter by price range
+              </Typography>
+              <Slider
+                valueLabelDisplay="auto"
+                max={maxPrice}
+                value={maxRangeValue}
+                onChange={handleSliderChange}
+                aria-labelledby="input-slider"
+              />
+            </Box>
+          </div>
+          <Autocomplete
+            freeSolo
+            value={catFilterValue}
+            onChange={(e, newValue) => setCatFilterValue(newValue)}
+            options={categories.map((category) => category.name)}
+            sx={{width:200}}
+            renderInput={(params) => <TextField {...params} label="Filter by category" />}
+          />
+        </div>
       </div>
       <div className="productsList">
-        {products?.length ? (
+        {displayProducts?.length ? (
           <> {renderProducts()}</>
         ) : (
-          <div className="catalogNoProducts">אין מוצרים בקטלוג!</div>
+          <div className="catalogNoProducts">No products available!</div>
         )}
       </div>
       <StatisticsDialog
