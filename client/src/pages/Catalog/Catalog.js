a036bb51b519259e5d60f12554013c45ab84ac7c
ofir-hazan <322815093@idf.il>
2023-02-15 14:45:29 +0200

    fix paths

--- client/src/pages/Catalog/Catalog.js
+++ client/src/pages/Catalog/Catalog.js
@@ -1,17 +1,17 @@
 import React, { useContext, useState } from "react";
 import "./Catalog.css";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 import Product from "../../Components/Product/Product";
 import StatisticsDialog from "../../Components/StatisticsDialog/StatisticsDialog";
-import BarChartIcon from '@mui/icons-material/BarChart';
+import BarChartIcon from "@mui/icons-material/BarChart";
 import { CartContext } from "../../Contexts/cartContext";
 import { GlobalContext } from "../../Contexts/GlobalContext";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { ROLES } from "../../resources/constants";
 
 function Catalog(props) {
   const { onAdd } = useContext(CartContext);
-  const { connectedUser } = useContext(GlobalContext)
+  const { connectedUser } = useContext(GlobalContext);
   const { products } = props;
   const [statsDialogOpen, setStatsDialogOpen] = useState(false);
 
@@ -25,22 +25,26 @@ function Catalog(props) {
     ));
   }
 
-  const ColorButton = styled(Button) ({
-    position: 'absolute',
-    color: 'white',
-    backgroundColor: '#6cbaa9',
-    '&:hover': {
-      backgroundColor: '#6cbaa9',
+  const ColorButton = styled(Button)({
+    position: "absolute",
+    color: "white",
+    backgroundColor: "#6cbaa9",
+    "&:hover": {
+      backgroundColor: "#6cbaa9",
     },
   });
 
   return (
     <div className="products">
       <div className="productsHeader">
-        {connectedUser?.role === ROLES.ADMIN &&
-          (<ColorButton onClick={() => setStatsDialogOpen(true)} startIcon={<BarChartIcon />}>
+        {connectedUser?.role === ROLES.ADMIN && (
+          <ColorButton
+            onClick={() => setStatsDialogOpen(true)}
+            startIcon={<BarChartIcon />}
+          >
             Watch statictics
-           </ColorButton>)}
+          </ColorButton>
+        )}
         <div className="productsTitle">רשימת מוצרים</div>
       </div>
       <div className="productsList">
@@ -50,7 +54,11 @@ function Catalog(props) {
           <div className="catalogNoProducts">אין מוצרים בקטלוג!</div>
         )}
       </div>
-      <StatisticsDialog open={statsDialogOpen} onClose={() => setStatsDialogOpen(false)} products/>
+      <StatisticsDialog
+        open={statsDialogOpen}
+        onClose={() => setStatsDialogOpen(false)}
+        products
+      />
     </div>
   );
 }
